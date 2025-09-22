d54ff32ab2d14bea9cb9b4e8cccfb70377a5582b
Ayush Ranjan <ayushranjan@google.com>
2025-09-21 19:58:58 -0700

    gofer: Invalidate dentry if file type changes but inode number doesn't.
    For some filesystems like ext4, it possible that the inode number is re-used in
    between unlink and file creation. Hence it is possible a file is replaced on
    the host filesystem with another file but the inode number remains the same.
    The gofer client can't detect such changes and trigger invalidation. But at
    least we can detect if the file type has changed and invalidate the old file.
    
    PiperOrigin-RevId: 809831715

--- pkg/sentry/fsimpl/gofer/lisafs_inode.go
+++ pkg/sentry/fsimpl/gofer/lisafs_inode.go
@@ -709,7 +709,9 @@ func doRevalidationLisafs(ctx context.Context, vfsObj *vfs.VirtualFilesystem, st
 		lastUnlockedDentry = i
 
 		// Note that synthetic dentries will always fail this comparison check.
-		if !found || d.inode.inoKey != inoKeyFromStatx(&stats[i]) {
+		if !found ||
+			d.inode.inoKey != inoKeyFromStatx(&stats[i]) ||
+			(stats[i].Mask&linux.STATX_TYPE != 0 && uint32(stats[i].Mode&linux.FileTypeMask) != d.inode.fileType()) {
 			d.inode.metadataMu.Unlock()
 			if !found && d.inode.isSynthetic() {
 				// We have a synthetic file, and no remote file has arisen to replace
@@ -720,13 +722,6 @@ func doRevalidationLisafs(ctx context.Context, vfsObj *vfs.VirtualFilesystem, st
 			// dentry invalidated.
 			d.invalidate(ctx, vfsObj, ds)
 			return nil
-		}
-
-		// Check that the file type has not changed.
-		if stats[i].Mask&linux.STATX_TYPE != 0 {
-			if got, want := stats[i].Mode&linux.FileTypeMask, d.inode.fileType(); uint32(got) != want {
-				panic(fmt.Sprintf("file type of %q changed from %#o to %#o while inode key (%+v) did not change", genericDebugPathname(d.inode.fs, d), want, got, d.inode.inoKey))
-			}
 		}
 
 		// The file at this path hasn't changed. Just update cached metadata.
